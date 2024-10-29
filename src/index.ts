11157fe1682b0159e19950fae78f2e2b4c2d7ad7
abubakar musa <abubakarbalamusa1@gmail.com>
2024-10-29 11:12:32 +0100

    minor ui fixes

--- src/index.ts
+++ src/index.ts
@@ -53,18 +53,20 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
-// console.log('Scheduling cron job...');
-// cron.schedule('* * * * *', async() => {
-//   console.log('Running a task every minute');
-//   const result = await axios.get('http://localhost:5000/api/v1/case/process',{
-//     headers: {
-//       'api-key': `${process.env.API_KEY}`
-//     }
-//   });
+console.log('Scheduling cron job...');
+cron.schedule('* * * * *', async() => {
+  console.log('Running a task every minute');
+  const result = await axios.get('http://localhost:5000/api/v1/case/process',{
+    headers: {
+      'api-key': `${process.env.API_KEY}`
+    }
+  });
 
-//   console.log(result?.data);
-//   // Add your task logic here
-// });
+  console.log(result?.data);
+  // Add your task logic here
+});
+
+
 console.log('Cron job scheduled.');
 
 const start = async (): Promise<void> => {
