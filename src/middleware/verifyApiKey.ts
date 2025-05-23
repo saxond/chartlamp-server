e907e06e985c32019e0b78457cc9ef455d18cbcb
veyron-davids <abiolaajileye@gmail.com>
2025-05-23 15:18:27 +0100

    fix

--- src/middleware/verifyApiKey.ts
+++ src/middleware/verifyApiKey.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 
 export const verifyApiKey = (req: Request, res: Response, next: NextFunction): void => {
+  return next(createHttpError(401, 'Unauthorized'));
   const apiKey = req.headers['api-key'];
   if (!apiKey || apiKey !== process.env.API_KEY) {
     return next(createHttpError(401, 'Unauthorized'));
